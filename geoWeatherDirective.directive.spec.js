4177ce0d36cf4e45c3a5aa773226c4c2c9911c4a
Ben Burton <sistematic3@yahoo.co.uk>
2015-02-23 17:33:20 +0000

    Initial commit

---
+++ geoWeatherDirective.directive.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Directive: geoWeatherDirective', function () {
+
+  // load the directive's module and view
+  beforeEach(module('oneStepBeyondApp'));
+  beforeEach(module('app/geoWeatherDirective/geoWeatherDirective.html'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($rootScope, $compile) {
+    scope = $rootScope.$new();
+    element = angular.element('<geo-weather-directive></geo-weather-directive>');
+    element = $compile(element)(scope);
+    scope.$apply();
+  }));
+
+  it('loading message should be Loading your weather...', function () {
+    expect(element.scope().weather.loadingMessage).toBe('Loading your weather...');
+  });
+
+  it('loading should be true on load', function () {
+    expect(element.scope().weather.loading).toBe(true);
+  });
+});
