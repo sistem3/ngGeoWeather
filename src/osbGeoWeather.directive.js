d4b276613eb411a078512ed55ffbc9e04a98db42
Ben Burton <sistematic3@yahoo.co.uk>
2016-01-10 14:08:28 +0000

    Switching Icon Detect to new system

--- src/osbGeoWeather.directive.js
+++ src/osbGeoWeather.directive.js
@@ -60,10 +60,9 @@ angular.module('sistem3.osb-geo-weather', ['osb-geo-weather-template'])
           $scope.weather.loading = true;
           $http.get($scope.weather.apiBase + 'weather?q=' + locationName + '&units=metric&APPID=' + $scope.weather.apiKey)
             .success(function(data) {
-              //console.log(data);
               $scope.weather.error = false;
               $scope.weather.today = data;
-              $scope.weather.today.icon = getWeatherIcon(data.weather[0].description);
+              $scope.weather.today.icon = '<i class="wi wi-owm-' + data.weather[0].id + '"></i>';
               $scope.weather.loading = false;
               checkDayNight($scope.weather.timeNow, $scope.weather.today.sys.sunrise, $scope.weather.today.sys.sunset);
             })
@@ -77,17 +76,15 @@ angular.module('sistem3.osb-geo-weather', ['osb-geo-weather-template'])
         var getForecast = function(locationName) {
           $http.get($scope.weather.apiBase + 'forecast/daily?q=' + locationName + '&units=metric&APPID=' + $scope.weather.apiKey)
             .success(function(data) {
-              //console.log(data);
               $scope.weather.forecast.error = false;
               $scope.weather.forecast.days = [];
               angular.forEach(data.list, function(key, value) {
-                key.icon = getWeatherIcon(key.weather[0].description);
+                key.icon = '<i class="wi wi-owm-' + key.weather[0].id + '"></i>';
                 this.push(key);
               }, $scope.weather.forecast.days);
               $scope.weather.forecast.loading = false;
             })
             .error(function(data) {
-              //console.log(data);
               $scope.weather.forecast.error = true;
               $scope.weather.forecast.loading = false;
             });
@@ -107,121 +104,6 @@ angular.module('sistem3.osb-geo-weather', ['osb-geo-weather-template'])
             $scope.weather.background = 'nightBg';
           } else {
             $scope.weather.background = 'defaultBg';
-          }
-        };
-        // Weather Icon checker (based on OpenWeatherApi statuses
-        var getWeatherIcon = function(weather) {
-          //console.log(weather);
-          switch(weather) {
-            case 'thunderstorm with light rain':
-            case 'thunderstorm with rain':
-            case 'thunderstorm with heavy rain':
-              return '<i class="wi wi-storm-showers"></i>';
-              break;
-            case 'light thunderstorm':
-            case 'thunderstorm with light drizzle':
-            case 'thunderstorm with drizzle':
-            case 'thunderstorm with heavy drizzle':
-              return '<i class="wi wi-day-storm-showers"></i>';
-              break;
-            case 'thunderstorm':
-              return '<i class="wi wi-day-thunderstorm"></i>';
-              break;
-            case 'heavy thunderstorm':
-            case 'ragged thunderstorm':
-              return '<i class="wi wi-thunderstorm"></i>';
-              break;
-            case 'light intensity drizzle':
-              return '<i class="wi wi-day-sprinkle"></i>';
-              break;
-            case 'drizzle':
-            case 'light intensity shower rain':
-              return '<i class="wi wi-sprinkle"></i>';
-              break;
-            case 'heavy intensity drizzle':
-            case 'light intensity drizzle rain':
-            case 'drizzle rain':
-            case 'shower drizzle':
-            case 'shower rain and drizzle':
-            case 'heavy intensity drizzle rain':
-            case 'heavy shower rain and drizzle':
-            case 'shower rain':
-            case 'heavy intensity shower rain':
-              return '<i class="wi wi-showers"></i>';
-              break;
-            case 'light rain':
-            case 'moderate rain':
-            case 'ragged shower rain':
-              return '<i class="wi wi-rain-mix"></i>';
-              break;
-            case 'heavy intensity rain':
-            case 'very heavy rain':
-              return '<i class="wi wi-rain"></i>';
-              break;
-            case 'extreme rain':
-              return '<i class="wi wi-rain-wind"></i>';
-              break;
-            case 'light snow':
-            case 'snow':
-            case 'heavy snow':
-            case 'light rain and snow':
-            case 'rain and snow':
-              return '<i class="wi wwi-snow"></i>';
-              break;
-            case 'sleet':
-            case 'shower sleet':
-            case 'light shower snow':
-            case 'shower snow':
-            case 'heavy shower snow':
-              return '<i class="wi wwi-sleet"></i>';
-              break;
-            case 'clear sky':
-              return '<i class="wi wi-day-sunny"></i>';
-              break;
-            case 'few clouds':
-            case 'scattered clouds':
-            case 'broken clouds':
-              return '<i class="wi wi-day-cloudy"></i>';
-              break;
-            case 'overcast clouds':
-              return '<i class="wi wi-cloudy"></i>';
-              break;
-            case 'tornado':
-              return '<i class="wi wi-tornado"></i>';
-              break;
-            case 'tropical storm':
-              return '<i class="wi wi-storm-showers"></i>';
-              break;
-            case 'hurricane':
-              return '<i class="wi wi-hurricane"></i>';
-              break;
-            case 'cold':
-              return '<i class="wi wi-snowflake-cold"></i>';
-              break;
-            case 'hot':
-              return '<i class="wi wi-hot"></i>';
-              break;
-            case 'windy':
-              return '<i class="wi wi-cloudy-windy"></i>';
-              break;
-            case 'hail':
-              return '<i class="wi wi-hail"></i>';
-              break;
-            case 'calm':
-              return '<i class="wi wi-day-sunny"></i>';
-              break;
-            case 'light breeze':
-            case 'gentle breeze':
-            case 'moderate breeze':
-            case 'fresh breeze':
-              return '<i class="wi wi-windy"></i>';
-              break;
-            case 'strong breeze':
-              return '<i class="wi wi-strong-wind"></i>';
-              break;
-            default:
-              return '<i class="wi wi-day-sunny"></i>';
-              break;
           }
         };
       }
